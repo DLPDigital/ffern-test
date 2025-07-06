10d30df28569f692c2440d96cc5ba3a92ee27982
Dom <72518625+DLPDigital@users.noreply.github.com>
2025-07-06 11:45:12 +0100

    feat: installing covik sans

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,24 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 const queryClient = new QueryClient()
 
+import localFont from "next/font/local"
+
+export const covikSans = localFont({
+  src: [
+    {
+      path: "../fonts/CovikSansDemo-Regular.woff2",
+      weight: "400",
+      style: "regular",
+    },
+  ],
+})
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
+    <div className={covikSans.className}>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </div>
   )
 }
